27b4bfc6cc9139186135877ab2cef765a014ef37
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-07-30 13:25:36 -0500

    Se crea lambaas para crud de user

---
+++ src/constants/response_messages.js
@@ -0,0 +1,8 @@
+class MessagesResponse {
+    static USER_CREATED = 'Usuario creado correctamente';
+    static USER_UPDATED = 'Usuario actualizado correctamente';
+    static USER_DELETED = 'Usuario eliminado correctamente';
+    static USER_NOT_FOUND = 'Usuario no encontrado';
+    static USERS_LIST = 'Lista de usuarios obtenida correctamente';
+}
+module.exports = MessagesResponse;
--- src/domain/services/user_service.js
+++ src/domain/services/user_service.js
@@ -1,11 +1,12 @@
 const users = require("../entities/users");
 const { v4: uuidv4 } = require("uuid");
 const ExceptionMessages = require("../../constants/exception_messages");
+
 class UserService {
   getUsers() {
     return users.getAll();
   }
-  crateUser(data) {
+  createUser(data) {
     const newUser ={
         id: uuidv4(), ...data
     };
@@ -15,9 +16,26 @@ class UserService {
   updateUser(id, data) {
     const userIndex = users.users.findIndex(user => user.id === id);
     if (userIndex === -1) {
-      throw new Error("User not found");
+      throw new Error(ExceptionMessages.USER_NOT_FOUND);
     }
     users.users[userIndex] = { ...users.users[userIndex], ...data };
     return users.users[userIndex];
   }
+
+    deleteUser(id) {
+        const userIndex = users.users.findIndex(user => user.id === id);
+        if (userIndex === -1) {
+        throw new Error(ExceptionMessages.USER_NOT_FOUND);
+        }
+        const deletedUser = users.users.splice(userIndex, 1);
+        return deletedUser[0];
+    }
+    getUserById(id) {
+        const user = users.users.find(user => user.id === id);
+        if (!user) {
+            throw new Error(ExceptionMessages.USER_NOT_FOUND);
+        }
+        return user;
+    }
 }
+module.exports = UserService;
